5a0bdc9f0a8f02d09d0c0623421b92bf7ba1fe17
Hridoy4 <Hridoy4@DESKTOP-9CDLVUQ>
2018-05-22 02:13:58 +0600

    Add Public/Css file and desing the comment section

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var seedDB = require("./seeds");
 
 var app = express();
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({
     extended: true
 }));

a8137f544a25c25d24fc9ff9abca9d7c786d1f8a
Hridoy4 <Hridoy4@DESKTOP-9CDLVUQ>
2018-05-23 23:45:14 +0600

    Add delete route to comments

--- routes/comments.js
+++ routes/comments.js
@@ -3,11 +3,6 @@ var express = require("express");
 var router = express.Router();
 var Campground = require("../models/campground");
 var Comment = require("../models/comment");
-
-//===================//
-//Comment section
-//===================//
-
 //Comments new
 
 router.get("/campgrounds/:id/comments/new",isLoggedIn, (req, res) => {
@@ -52,6 +47,47 @@ router.post("/campgrounds/:id/comments", isLoggedIn,(req, res) => {
     //connect new commnet to campground
     //redirect to camground show page
 });
+//edit route
+router.get("/campgrounds/:id/comments/:comment_id/edit", (req,res) =>{
+    var campground_id = req.params.id;
+    var comment_id = req.params.comment_id;
+    Comment.findById(comment_id,(err,comment) =>{
+        if(err) {
+            res.redirect("back");
+        } else {
+            res.render("comments/edit",{campground_id, comment});
+        }
+    });
+});
+//Update comment
+router.put("/campgrounds/:id/comments/:comment_id", (req, res) =>{
+        var campId = req.params.id;
+        var id = req.params.comment_id;
+        Comment.findByIdAndUpdate(id, req.body.comment, (err, comment) =>{
+            if(err) {
+                res.send("failed");
+            } else {
+                console.log("Problme is here");
+                res.redirect("/campgrounds/"+campId);
+            }
+            
+        });
+});
+//Comments delete route
+router.delete("/campgrounds/:id/comments/:comment_id", (req,res) =>{
+    var id = req.params.comment_id;
+    var campId = req.params.id;
+    Comment.findOneAndRemove(id,(err) =>{
+        if (err) {
+            console.log(err);
+            res.redirect("back");
+        } else {
+            console.log("deleted");
+            res.redirect("/campgrounds/"+campId);
+        }
+    });
+});
+
 //Authorization middleware
 
 function isLoggedIn (req,res,next) {

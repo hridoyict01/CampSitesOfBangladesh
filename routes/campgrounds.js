63e31e4180b9657f23981617acf88d5efe992b1a
Hridoy4 <Hridoy4@DESKTOP-9CDLVUQ>
2018-05-23 21:53:11 +0600

    Add authorization to edit and delete

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -64,29 +64,24 @@ router.get("/campgrounds/:id", (req, res) => {
 
 });
 //Edit campground route
-router.get("/campgrounds/:id/edit",(req,res) => {
-    var id = req.params.id;
-    Campground.findById(id, (err, campground) =>{
-        if(err) {
-            res.redirect("/campgrounds");
-        } else {
+router.get("/campgrounds/:id/edit",checkCampgroundOwnership, (req, res) => {
+        var id = req.params.id;
+        Campground.findById(id, (err, campground) => {
             res.render("campgrounds/edit", {campground});
-        }
-    });
-    
+        });
 });
 //Update campground route
-router.put("/campgrounds/:id", (req,res) => {
+router.put("/campgrounds/:id",checkCampgroundOwnership, (req, res) => {
     //find and update the campground
     var id = req.params.id;
     console.log(id);
-    Campground.findByIdAndUpdate(id, req.body.campground,(err, campground) =>{
-        if(err) {
+    Campground.findByIdAndUpdate(id, req.body.campground, (err, campground) => {
+        if (err) {
             console.log(err);
             res.redirect("/campgrounds");
         } else {
             console.log(campground);
-            res.redirect("/campgrounds/"+id);
+            res.redirect("/campgrounds/" + id);
         }
     });
 
@@ -95,10 +90,10 @@ router.put("/campgrounds/:id", (req,res) => {
 
 //Destroy campground route
 
-router.delete("/campgrounds/:id", (req,res) =>{
+router.delete("/campgrounds/:id",checkCampgroundOwnership, (req, res) => {
     var id = req.params.id;
-    Campground.findByIdAndRemove(id, (err, campground) =>{
-        if(err) {
+    Campground.findByIdAndRemove(id, (err, campground) => {
+        if (err) {
             console.log(err);
             res.redirect("/campgrounds");
         } else {
@@ -106,17 +101,37 @@ router.delete("/campgrounds/:id", (req,res) =>{
         }
     });
 });
-
+//cheack user ownershiop
+function checkCampgroundOwnership(req,res, next) {
+      //is user logged in
+      if (req.isAuthenticated()) {
+        var id = req.params.id;
+        Campground.findById(id, (err, campground) => {
+            if (err) {
+                res.redirect("/campgrounds");
+            } else {
+                if(campground.author.id.equals(req.user._id) ){
+                   next();
+                } else {
+                    res.redirect("back");
+                }
+            
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+}
 
 
 
 
 //Authorization middlewire
 
-function isLoggedIn (req,res,next) {
-    if(req.isAuthenticated()) {
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
         return next();
-    } 
+    }
     res.redirect("/login");
 }
 
